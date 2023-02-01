8551f612b66f0ef2343cedd2e8ed80ca2467bd85
Antonio Thiago <antoniobackenddeveloper@gmail.com>
2023-02-01 19:28:41 -0300

    setting environment variables

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 const app = express();
 import connectDatabase from "./src/database/db.js";
 
 import userRoute from "./src/routes/user.route.js";
-const port = 8089;
 
 connectDatabase();
 app.use(express.urlencoded({ extended: false }));
@@ -11,4 +12,6 @@ app.use(express.json());
 
 app.use("/user", userRoute);
 
-app.listen(8089, () => console.log(`Server running on port ${port}`));
+app.listen(8089, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+);
--- src/database/db.js
+++ src/database/db.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const connectDatabase = () => {
   console.log("Trying to connect to the database...");
   mongoose
-    .connect(
-      "mongodb+srv://root:root@cluster0.bvgk3l3.mongodb.net/?retryWrites=true&w=majority",
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    )
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then((response) => {
       console.log("Mongodb Atlas successfully connected!");
     })
