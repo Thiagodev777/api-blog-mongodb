510233d139d93a984535b5257ff544089c85e607
Antonio Thiago <antoniobackenddeveloper@gmail.com>
2023-02-04 23:49:18 -0300

    documentation swagger

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import connectDatabase from "./src/database/db.js";
 import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
 import newsRoute from "./src/routes/news.route.js";
+import swaggerRoute from "./src/routes/swagger.route.js";
 
 connectDatabase();
 app.use(express.urlencoded({ extended: false }));
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/news", newsRoute);
+app.use("/doc", swaggerRoute);
 
 app.listen(8089, () =>
   console.log(`Server running on port ${process.env.PORT || 8089}`)
